80df0077615385321d12edc1279ef4748aef8a68
Commander-Crash <86231294+Commander-Crash@users.noreply.github.com>
2023-10-31 00:03:09 -0500

    Update ble_detect.sh

--- BLE_MAC_Detection/ble_detect.sh
+++ BLE_MAC_Detection/ble_detect.sh
@@ -1,36 +1,57 @@
-#!/bin/bash
+import subprocess
+import time
 
-hciconfig hci0 down
-hciconfig hci0 up
+# Define the MAC addresses and their respective actions
+mac_actions = {
+    '00:00:00:00:00:00': [
+        'path/to/script_or_commmand'
+    ],
+    '00:00:00:00:00:00': [
+        'path/to/script_or_commmand'
+    ],
+    '00:00:00:00:00:00': [
+        'path/to/script_or_commmand'
+    ],
+    '00:00:00:00:00:00': [
+        'path/to/script_or_commmand'
+    ],
+    '00:00:00:00:00:00': [
+        'path/to/script_or_commmand'
+    ]
+    # Add more MAC addresses and their corresponding scripts as needed
+}
 
-logfile="ble.log"
-option=$1
+while True:
+    # Restart the Bluetooth adapter
+    subprocess.run(['hciconfig', 'hci0', 'down'])
+    subprocess.run(['hciconfig', 'hci0', 'up'])
 
-if [  "$option" == "--mac" ]; then
-  mac=$2
-  # Run hcitool lescan for 10 sec and save output to log file
-  timeout 10 stdbuf -oL hcitool lescan > $logfile
-  # Kill hcitool lescan after specified time
-  pkill --signal SIGINT hcitool
-  # Look for MAC addresses and save them in new array
-  mac_list=($(grep -oE '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' $logfile))
+    logfile = "ble.log"
+    option = "--mac"
+    mac = None
 
-  if [ -n "$mac" ]; then
-    echo "Searching for $mac"
-    # Iterate through list and find somparison to MAC provided in arguement
-    for i in "${mac_list[@]}"
-    do
-      if [ "$i" == "$mac" ]; then
-        echo "BLE Mac found: $i"
-        # INSERT_CUSTOM_COMMAND_HERE #
-        exit 0
-      fi
-    done
-    echo "No BLE matching MAC Found."
-  else
-    echo "No matching MAC address found in scan."
-  fi
+    # Run hcitool lescan for 10 sec and save output to log file
+    scan_process = subprocess.Popen(['timeout', '10', 'stdbuf', '-oL', 'hcitool', 'lescan'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-else
-  echo "Error: Please specify valid argument (--mac)"
-fi
+    printed_info = set()  # Keep track of printed MAC addresses
+
+    for line in scan_process.stdout:
+        line = line.decode('utf-8').strip()
+        for mac_address, action_scripts in mac_actions.items():
+            if mac_address in line and mac_address not in printed_info:
+                # Display the MAC address and name of the device (if available) in green text
+                print('\033[92m' + line + '\033[0m')
+
+                # Execute both Python scripts and display the output in blue text
+                for action_script in action_scripts:
+                    script_process = subprocess.Popen(['python3', action_script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    script_output = script_process.stdout.read().decode('utf-8')
+                    print('\033[91m' + script_output + '\033[0m')
+
+                printed_info.add(mac_address)  # Add MAC address to printed set
+
+    # Kill hcitool lescan after specified time
+    scan_process.kill()
+
+    # Wait for 10 seconds before rescanning
+    time.sleep(10)
